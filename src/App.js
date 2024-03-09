6c9faa8d850c846c993f1d8951ef302e65b2ddd7
Corey <coreygans@gmail.com>
2024-03-09 11:52:29 -0600

    small styling updates, added description for product and starting to add header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import ProductList from "./components/ProductList";
 
 function App() {
   const products = [
-    { id: 1, name: "Cherries", price: 3.45, image: "/images/cherries.jpg" },
-    { id: 2, name: "Coffee", price: 4.89, image: "/images/coffee.jpg" },
-    { id: 1, name: "Corn", price: 1.25, image: "/images/corn.jpg" },
-    { id: 2, name: "Olive Oil", price: 5.29, image: "/images/olive-oil.jpg" },
-    { id: 1, name: "Raspberries", price: 4.25, image: "/images/raspberries.jpg" },
-    { id: 2, name: "Strawberries", price: 3.79, image: "/images/strawberries.jpg" },
+    { id: 1, name: "Cherries", details: "The best cherries around. Fresh picked and ready for you to eat.", price: 3.45, image: "/images/cherries.jpg" },
+    { id: 2, name: "Coffee", details: "Roasted coffee beans from Columbia.", price: 4.89, image: "/images/coffee.jpg" },
+    { id: 3, name: "Corn", details: "Sweet corn fresh from the farm.", price: 1.25, image: "/images/corn.jpg" },
+    { id: 4, name: "Olive Oil", details: "Olive Oil imported from Italy.", price: 5.29, image: "/images/olive-oil.jpg" },
+    { id: 5, name: "Raspberries", details: "Sweet and Tart raspeberries.", price: 4.25, image: "/images/raspberries.jpg" },
+    { id: 6, name: "Strawberries", details: "Juicy strawberries for a summer treat.", price: 3.79, image: "/images/strawberries.jpg" },
   ];
 
   const addToCart = (product) => {
@@ -18,8 +18,9 @@ function App() {
 
   return (
     <div className="app">
-      
+
       <h1>The Food Store</h1>
+      <header> <div>Home </div></header>
       <div className="product-list">
       <ProductList products={products} addToCart={addToCart} />
       </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,9 +3,10 @@ import {useState} from "react";
 function Product({ product, addToCart }) {
   return (
     <div className="product" key={product.id}>
-      <img className="product" src={product.image} alt={product.name} />
+      <img className="productImg" src={product.image} alt={product.name} />
       <h2>{product.name}</h2>
-      <p>Price: ${product.price}</p>
+      <p><b>Description:</b> {product.details}
+        <br/> <b>Price:</b> ${product.price}</p>
       <button
         className="add-to-cart-button"
         onClick={() => addToCart(product)}>
